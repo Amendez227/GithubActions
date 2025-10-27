2a696969522aeb22256b8d04bd5d38304fbb5107
Amendez227 <amendez14000@gmail.com>
2025-10-27 15:27:39 -0500

    bruh

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,7 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
+
 		var baseValue = double.Parse(x);
 		var exponent  = double.Parse(y);
 
