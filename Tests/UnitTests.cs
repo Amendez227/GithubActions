07f0fab8fc4c7add72a8bada06679ba003ce06e2
Amendez227 <amendez14000@gmail.com>
2025-10-27 22:34:37 -0500

    dudeBeFR

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -3,59 +3,60 @@ namespace GithubActionsLab;
 [TestClass]
 public class Addition
 {
-    [TestMethod]
-    public void Add_Valid_Patino()
-    {
-        Assert.AreEqual(3, Program.Add("1", "2"));
-        Assert.AreEqual(5, Program.Add("3", "2"));
-        Assert.AreEqual(12, Program.Add("5", "7"));
-    }
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		Assert.AreEqual(3, Program.Add("1", "2"));
+		Assert.AreEqual(5, Program.Add("3", "2"));
+		Assert.AreEqual(12, Program.Add("5", "7"));
+	}
 
-        [TestMethod]
-        public void Subtract()
-        {
-            // Subtract
-            Assert.AreEqual(-1, Program.Subtract("1", "2"));
-        Assert.AreEqual(1, Program.Subtract("3", "2"));
-        Assert.AreEqual(-2, Program.Subtract("5", "7"));
-    }
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		// Subtract
+		Assert.AreEqual(-1, Program.Subtract("1", "2"));
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(-2, Program.Subtract("5", "7"));
+	}
 
-        [TestMethod]
-        public void Multiply()
-        {
-            // Multiply
-            Assert.AreEqual(2, Program.Multiply("1", "2"));
-        Assert.AreEqual(6, Program.Multiply("3", "2"));
-        Assert.AreEqual(35, Program.Multiply("5", "7"));
-    }
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		// Multiply
+		Assert.AreEqual(2, Program.Multiply("1", "2"));
+		Assert.AreEqual(6, Program.Multiply("3", "2"));
+		Assert.AreEqual(35, Program.Multiply("5", "7"));
+	}
 
-        [TestMethod]
-        public void Divide()
-        {
-            // Divide
-            Assert.AreEqual(0.5, Program.Divide("1", "2"));
-        Assert.AreEqual(1.5, Program.Divide("3", "2"));
-        Assert.AreEqual(0.7142857, Program.Divide("5", "7"), 0.0001); // with delta for floating point precision
-    }
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		// Divide
+		Assert.AreEqual(0.5, Program.Divide("1", "2"));
+		Assert.AreEqual(1.5, Program.Divide("3", "2"));
+		Assert.AreEqual(0.7142857, Program.Divide("5", "7"), 0.0001); // with delta for floating point precision
+	}
 
-        [TestMethod]
-        public void Power()
-        {
-            // Power
-            Assert.AreEqual(1, Program.Power("1", "2"));
-        Assert.AreEqual(9, Program.Power("3", "2"));
-        Assert.AreEqual(78125, Program.Power("5", "7"));
+	[TestMethod]
+	public void Add_Valid_Patino()
+	{
+		// Power
+		Assert.AreEqual(1, Program.Power("1", "2"));
+		Assert.AreEqual(9, Program.Power("3", "2"));
+		Assert.AreEqual(78125, Program.Power("5", "7"));
+	}
 
-    }
 
-    [TestMethod]
+
+	[TestMethod]
 	public void Add_Invalid_Patino()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
 	}
-	:./.l./.,/
+
 	[TestMethod]
 	public void Add_Null_Patino()
 	{
